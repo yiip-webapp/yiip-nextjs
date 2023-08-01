455a40afae0adb0728ccf9d423ce9d44953722b5
amanleykj <118389631+amanleykj@users.noreply.github.com>
2023-08-01 13:38:57 +0900

    front end development

--- server/models/business.model.js
+++ server/models/business.model.js
@@ -6,42 +6,48 @@ const BusinessSchema = new mongoose.Schema({
         required: [
             true,
             "Every business specializes in an industry!"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     },
     name: {
         type: String,
         required: [
             true,
             "Every business has a name!"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     },
     address: {
         type: String,
         required: [
             true,
             "Every business has a location! (If it's an online business, put online.)"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     },
     phone: {
         type: String,
         required: [
             true,
             "Every business has a phone number!"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     },
     hours: {
         type: String,
         required: [
             true,
             "Every business has business hours!"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     },
     service: {
         type: String,
         required: [
             true,
             "Every business offers a product/service!"
-        ]
+        ],
+        minLength : [3, "This must be at least three characters long."]
     }
 }, {timestamps: true});
 
